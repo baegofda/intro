a00d8686f0ead91af1e88c94dc5d8c9b6658055f
baegofda <molly.daleo3o@gmail.com>
2022-03-13 23:05:20 +0900

    chore: 다크모드 컬러 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   darkMode: 'class',
   content: ['./pages/**/*.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // #64748b
+        'main-content': '#0f172a',
+        'd-main-content': '#fff',
+        'sub-content': '#334155',
+        'd-sub-content': '#94a3b8',
+      },
+      backgroundColor: {
+        light: '#fff',
+        dark: '#1a1b1e',
+      },
+    },
   },
   plugins: [],
 }
